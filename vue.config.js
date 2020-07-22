607f5d7a8c804bd8265b0492c829f95dedd42715
Ken Stanley <support@dohpaz.dev>
2020-07-22 08:25:10 -0400

    Add the package version as environment variable

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,10 @@ module.exports = {
       title: 'Quarkboard Admin'
     }
   },
+  chainWebpack: config => {
+    config.plugin('define').tap(definitions => {
+      definitions[0]['process.env']['PACKAGE_VERSION'] = JSON.stringify(require('./package.json').version);
+      return definitions;
+    });
+  },
 };
